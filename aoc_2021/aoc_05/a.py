db86498986954c6bb2194f6db7245a74ad72cd55
Jernej Habjan <jhabjan@marinexchange.com>
2022-12-07 19:22:19 +0100

    2021 5a rework

--- aoc_2021/aoc_05/a.py
+++ aoc_2021/aoc_05/a.py
@@ -1,57 +1,70 @@
+import numpy as np
+
+
+def get_tuples(lines):
+    tuples = []
+    for b in lines:
+        first, second = b.split(" -> ")
+        x1str, y1str = first.split(",")
+        x2str, y2str = second.split(",")
+        x1, y1, x2, y2 = int(x1str), int(y1str), int(x2str), int(y2str)
+        tuples.append((x1, y1, x2, y2))
+    return tuples
+
+
+def find_max_x_y(tuples):
+    # find max nr
+    total_max_x = 0
+    total_max_y = 0
+    for x1, y1, x2, y2 in tuples:
+        if max(x1, x2) > total_max_x:
+            total_max_x = max(x1, x2)
+        if max(y1, y2) > total_max_y:
+            total_max_y = max(y1, y2)
+    return total_max_x + 1, total_max_y + 1
+
+
+def fill_array_vertical_horizontal(array, tuples):
+    for x1, y1, x2, y2 in tuples:
+        if x1 == x2:
+            # vertical line
+            min_y = min(y1, y2)
+            max_y = max(y1, y2)
+            for i in range(min_y, max_y + 1):
+                array[i, x1] += 1
+                # print("increased x")
+        if y1 == y2:
+            # vertical line
+            min_x = min(x1, x2)
+            max_x = max(x1, x2)
+            for i in range(min_x, max_x + 1):
+                array[y1, i] += 1
+                # print("increased y")
+        # print(array)
+        # print("")
+
+
+def count_sum(array):
+    overlap_nr = 2
+    total = 0
+    for row in array:
+        for ele in row:
+            if ele >= overlap_nr:
+                total += 1
+    return total
+
+
 def aoc_2021_05_a():
     import pathlib
 
     with open(str(pathlib.Path(__file__).parent.resolve()) + '\\input.txt', "r") as f:
         lines = f.read().splitlines()
-
-    # find max nr
-    total_max_x = 0
-    total_max_y = 0
-    for b in lines:
-        first, second = b.split(" -> ")
-        x1str, y1str = first.split(",")
-        x2str, y2str = second.split(",")
-        x1, y1, x2, y2 = int(x1str), int(y1str), int(x2str), int(y2str)
-        if max(x1, x2) > total_max_x:
-            total_max_x = max(x1, x2)
-        if max(y1, y2) > total_max_y:
-            total_max_y = max(y1, y2)
-
-    # initialize empty array
-    arr = []
-    for i in range(0, total_max_x + 1):
-        arr.append([])
-        for j in range(0, total_max_y + 1):
-            arr[i].append(0)
-
-    # iterate
-    for b in lines:
-        first, second = b.split(" -> ")
-        x1str, y1str = first.split(",")
-        x2str, y2str = second.split(",")
-        x1, y1, x2, y2 = int(x1str), int(y1str), int(x2str), int(y2str)
-        if x1 == x2:
-            # vertical line
-            miny = min(y1, y2)
-            maxy = max(y1, y2)
-            for i in range(miny, maxy + 1):
-                arr[x1][i] += 1
-                # print("increased x")
-        if y1 == y2:
-            # vertical line
-            minx = min(x1, x2)
-            maxx = max(x1, x2)
-            for i in range(minx, maxx + 1):
-                arr[i][y1] += 1
-                # print("increased y")
-    summo = 0
-    for l in arr:
-        for l1 in l:
-            if l1 >= 2:
-                summo += 1
-            # print(l1)
-        # print(("".join(str(x) for x in l)).replace("0", "."))
-    return summo
+    tuples = get_tuples(lines)
+    max_x, max_y = find_max_x_y(tuples)
+    array = np.full((max_x, max_y), 0)
+    fill_array_vertical_horizontal(array, tuples)
+    total = count_sum(array)
+    return total
 
 
 if __name__ == '__main__':
