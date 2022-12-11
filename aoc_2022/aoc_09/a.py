364ec6d49e42b2402fc09438b00f8d4b1630d88a
Jernej Habjan <jhabjan@marinexchange.com>
2022-12-11 19:07:03 +0100

    2022 9 rework for b

--- aoc_2022/aoc_09/a.py
+++ aoc_2022/aoc_09/a.py
@@ -2,32 +2,33 @@ import math
 
 
 class Tail:
-    _current_x = 0
-    _current_y = 0
+    current_x = 0
+    current_y = 0
     visited_coordinates: set[tuple[int, int]] = set()
 
     def __init__(self):
-        self.visited_coordinates.add((self._current_x, self._current_y))
+        self.visited_coordinates.add((self.current_x, self.current_y))
 
-    def move_tail_next_to_head(self, head_x, head_y, prev_head_x, prev_head_y, direction):
-        dist = math.dist([head_x, head_y], [self._current_x, self._current_y])
-        # print("new head_x", head_x, "new head_y", head_y, "_current_x", self._current_x, "_current_y", self._current_y,
-        #       "DIST:",
-        #       dist)
+    def move_tail(self, head_x, head_y, prev_head_x, prev_head_y, direction):
+        dist = math.dist([head_x, head_y], [self.current_x, self.current_y])
+        # print("new head_x", head_x, "new head_y", head_y, "_current_x", self._current_x, "_current_y",
+        # self._current_y, "DIST:", dist)
 
         if dist == 1:
             return
 
         are_touching = dist < 2
 
-        same_row_or_col = head_x == self._current_x or head_y == self._current_y
+        same_row_or_col = head_x == self.current_x or head_y == self.current_y
 
-        if not are_touching:
+        if are_touching:
+            new_x = self.current_x
+            new_y = self.current_y
+        else:
             if not same_row_or_col:
                 # move diagonally
-                new_x = prev_head_x  # todo
-                new_y = prev_head_y  # todo
-                # print("not touching and diff row or col")
+                new_x = prev_head_x
+                new_y = prev_head_y
 
             else:
                 # print("not touching and same row or col")
@@ -44,33 +45,21 @@ class Tail:
                 else:  # direction "D"
                     new_x = head_x
                     new_y = head_y - 1
-        else:
-            if not same_row_or_col:
-                # print("are touching and not same row and col")
-                # todo move diagonally if lagging behind
-                new_x = self._current_x  # todo
-                new_y = self._current_y  # todo
 
-            else:
-                # print("are touching and same row and col")
-                new_x = self._current_x  # todo
-                new_y = self._current_y  # todo
+        self.current_x = new_x
+        self.current_y = new_y
 
-        self._current_x = new_x
-        self._current_y = new_y
-
-        self.visited_coordinates.add((self._current_x, self._current_y))
+        self.visited_coordinates.add((self.current_x, self.current_y))
 
     def get_debug_pos(self):
-        return self._current_x, self._current_y
+        return self.current_x, self.current_y
 
 
 class Head:
-    _current_x = 0
-    _current_y = 0
-    _tail = Tail()
+    current_x = 0
+    current_y = 0
 
-    def move_head_multiple_steps(self, direction, amount):
+    def move_head(self, direction):
         if direction == "R":
             increase_factor = (1, 0)
         elif direction == "L":
@@ -81,32 +70,15 @@ class Head:
             increase_factor = (0, 1)
 
         move_x, move_y = increase_factor
-        for i in range(amount):
-            new_x = self._current_x + move_x
-            new_y = self._current_y + move_y
-            self._move_head(new_x, new_y, direction)
 
-    def _move_head(self, new_x, new_y, direction):
-        prev_head_x = self._current_x
-        prev_head_y = self._current_y
-        self._current_x = new_x
-        self._current_y = new_y
-        self._tail.move_tail_next_to_head(self._current_x, self._current_y, prev_head_x, prev_head_y, direction)
-
-    def get_tail_visited_coordinates_count(self) -> int:
-        return len(self._tail.visited_coordinates)
-
-    def get_tail_visited_coordinates(self):
-        return self._tail.visited_coordinates
+        self.current_x += move_x
+        self.current_y += move_y
 
     def get_debug_pos(self):
-        return self._current_x, self._current_y
-
-    def get_debug_pos_tail(self):
-        return self._tail.get_debug_pos()
+        return self.current_x, self.current_y
 
 
-def aoc_2022_09_a():
+def prepare_movements():
     import pathlib
     with open(str(pathlib.Path(__file__).parent.resolve()) + '\\input.txt', "r") as f:
         lines = f.read().splitlines()
@@ -115,15 +87,20 @@ def aoc_2022_09_a():
     for line in lines:
         a, b = line.split()
         movements.append((a, int(b)))
+    return movements
 
+
+def aoc_2022_09_a():
+    movements = prepare_movements()
     head = Head()
+    tail = Tail()
     for direction, amount in movements:
-        head.move_head_multiple_steps(direction, amount)
-        # print("#################### Head debug pos", head.get_debug_pos(), "tail debug pos", head.get_debug_pos_tail(),
-        #       "tail coordinates",
-        #       head.get_tail_visited_coordinates())
+        for _ in range(amount):
+            prev_head_x, prev_head_y = head.current_x, head.current_y
+            head.move_head(direction)
+            tail.move_tail(head.current_x, head.current_y, prev_head_x, prev_head_y, direction)
 
-    visited_count = head.get_tail_visited_coordinates_count()
+    visited_count = len(tail.visited_coordinates)
 
     return visited_count
 
--- aoc_2022/aoc_09/b.py
+++ aoc_2022/aoc_09/b.py
@@ -1,9 +1,26 @@
-def aoc_2022_09_b():
-    import pathlib
-    with open(str(pathlib.Path(__file__).parent.resolve()) + '\\input.txt', "r") as f:
-        lines = f.read().splitlines()
+import aoc_2022.aoc_09.a as aoc_09_a
 
-    return 0
+
+def aoc_2022_09_b():
+    movements = aoc_09_a.prepare_movements()
+    head = aoc_09_a.Head()
+    tails: list[aoc_09_a.Tail] = [aoc_09_a.Tail() for _ in range(9)]
+    print(tails)
+    for direction, amount in movements:
+        for _ in range(amount):
+            prev_head_x, prev_head_y = head.current_x, head.current_y
+            head.move_head(direction)
+
+            prev_current_tail_x, prev_current_tail_y = tails[0].current_x, tails[0].current_y
+            tails[0].move_tail(head.current_x, head.current_y, prev_head_x, prev_head_y, direction)
+
+            for i in range(1, 9):
+                tails[i].move_tail(tails[i - 1].current_x, tails[i - 1].current_y, prev_current_tail_x,
+                                   prev_current_tail_y, direction)
+
+    visited_count = len(tails[8].visited_coordinates)
+
+    return visited_count
 
 
 if __name__ == '__main__':
